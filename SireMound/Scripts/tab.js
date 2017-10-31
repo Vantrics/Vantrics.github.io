8b68016c4ff95f72291f2012009317bdbfab73c4
Vantrics <killingmedj1993@gmail.com>
2017-10-31 12:05:26 +0800

    更新垃圾～
    更新垃圾～

--- SireMound/Scripts/tab.js
+++ SireMound/Scripts/tab.js
@@ -1,4 +1,4 @@
-﻿function openCity(evt, cityName) {
+﻿function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -8,7 +8,7 @@
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
 
